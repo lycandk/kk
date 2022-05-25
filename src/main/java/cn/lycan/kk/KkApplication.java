c95a380654933f942bd35a39fbdc658c5bc6e619
Makkapakka <1961605387@qq.com>
2022-05-25 17:52:28 +0800

    初始化项目构建版本提交

---
+++ src/main/java/cn/lycan/kk/KkApplication.java
@@ -0,0 +1,16 @@
+package cn.lycan.kk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Makkapakka
+ */
+@SpringBootApplication
+public class KkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lycan/kk/KkApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lycan.kk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
