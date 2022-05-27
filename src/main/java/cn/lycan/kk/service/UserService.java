716242a5168ad68a2496423d9ede8d1d99ea3a70
Makkapakka <1961605387@qq.com>
2022-05-27 09:35:43 +0800

    UserService.java增加事务注解

--- src/main/java/cn/lycan/kk/service/UserService.java
+++ src/main/java/cn/lycan/kk/service/UserService.java
@@ -7,6 +7,7 @@ import com.baomidou.mybatisplus.core.conditions.segments.MergeSegments;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Makkapakka
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 @Log4j2
+@Transactional
 public class UserService {
     @Autowired
     UserMapper userMapper;
