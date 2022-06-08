51b6fffc3451e637281d0e3225eb248d17f11f98
Makkapakka <1961605387@qq.com>
2022-06-08 11:30:07 +0800

    增加AdminMenuService方法执行日志打印

--- src/main/java/cn/lycan/kk/service/AdminMenuService.java
+++ src/main/java/cn/lycan/kk/service/AdminMenuService.java
@@ -5,6 +5,7 @@ import cn.lycan.kk.entity.AdminMenu;
 import cn.lycan.kk.entity.AdminRoleMenu;
 import cn.lycan.kk.entity.AdminUserRole;
 import cn.lycan.kk.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,7 @@ import java.util.stream.Collectors;
  * 根据 parentId 把子菜单放进父菜单对象中，整理返回有正确层级关系的菜单数据
  */
 @Service
+@Slf4j
 public class AdminMenuService {
     
     @Autowired
@@ -47,25 +49,29 @@ public class AdminMenuService {
      * @return
      */
     public List<AdminMenu> getMenusByCurrentUser() {
-        
+    
         //Get Current User in DB
         String userName = SecurityUtils.getSubject().getPrincipal().toString();
         User user = userService.getByName(userName);
-        
+    
         //Get roles' ids of current user
         //使用了 stream 来简化列表的处理，包括使用 map() 提取集合中的某一属性
         List<Integer> rids = adminUserRoleService.findAllByUid(user.getId()).stream().map(AdminUserRole::getRid).collect(Collectors.toList());
-        
+        log.info("根据用户：" + user.getUsername() + "获取角色id: " + rids.toString());
+    
         //Get Menu items of these roles
         //通过 distinct() 对查询出的菜单项进行了去重操作，避免多角色情况下有冗余的菜单
         List<Integer> mids = adminRoleMenuService.findAllByRidIn(rids).stream().map(AdminRoleMenu::getMid).collect(Collectors.toList());
         List<AdminMenu> menus = adminMenuDAO.findAllById(mids).stream().distinct().collect(Collectors.toList());
-        
+        log.info("根据用户：" + user.getUsername() + "获取菜单id: " + mids.toString());
+        log.info("根据菜单id：" + rids.toString() + "获取菜单: " + menus.toString());
+    
         //adjust the menu structure
         //调整菜单结构
         handleMenus(menus);
+        log.info("调整菜单结构为：" + menus);
         return menus;
-        
+    
     }
     
     public List<AdminMenu> getMenusByRoleId(int rid) {
